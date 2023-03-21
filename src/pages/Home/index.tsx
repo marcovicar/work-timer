9e2d68f15865452c663b93cdf62cedd7cc918a08
marcovicar <marcosvictormbi85@gmail.com>
2023-03-21 20:49:50 -0300

    datalist adicionado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome ao seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome ao seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto Stark" />
+            <option value="Projeto CoronaDen" />
+            <option value="Projeto Avengers" />
+            <option value="Projeto Top Secret" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
